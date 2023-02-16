5bb2f929786e56b3f28ac26c4e3cfc192cbc48f2
SphericalKat <amolele@gmail.com>
2023-02-16 22:29:59 +0530

    fix: URL shortened previews
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,13 @@ export interface Env {
 
 const router = new Router<Env>()
 
-const customKeyModifier = (id: string) => (request: Request) => {
+const customKeyModifier = (id: string, template: string) => (request: Request) => {
     let url = request.url
     //custom key mapping optional
-    url = url.replace(`${id}`, 'paste.html');
+    url = url.replace('/v', '').replace(`${id}`, template);
     return mapRequestToAsset(new Request(url, request))
 }
+
 
 router.post('/', async ({ req, res, env }) => {
     const { content } = queryString.parse(req.body);
@@ -28,7 +29,12 @@ router.post('/', async ({ req, res, env }) => {
         .bind(id, content)
         .run();
 
-    res.raw = Response.redirect(`${new URL(req.url).origin}/${id}`, 302)
+    try {
+        new URL(content!.toString())
+        res.raw = Response.redirect(`${new URL(req.url).origin}/v/${id}`, 302)
+    } catch (err) {
+        res.raw = Response.redirect(`${new URL(req.url).origin}/${id}`, 302)
+    }
     return;
 });
 
@@ -49,12 +55,42 @@ router.get('/:id', async ({ req, res, env }) => {
     const asset = await getAssetFromKV(req.raw, {
         ASSET_NAMESPACE: env.__STATIC_CONTENT,
         ASSET_MANIFEST: assetManifest,
-        mapRequestToAsset: customKeyModifier(id),
+        mapRequestToAsset: customKeyModifier(id, 'paste.html'),
     })
 
     try {
         const url = new URL(result.content)
         res.raw = Response.redirect(url.origin, 302);
+        return;
+    } catch (e) {
+        res.headers.set("Content-Type", "text/html");
+        res.body = Mustache.render(await asset.text()!, { paste: result.content, id });
+        return;
+    }
+})
+
+router.get('/v/:id', async ({ req, res, env }) => {
+    const { id } = req.params;
+
+    const resp = await env.DB.prepare("SELECT * FROM pastes WHERE id = ?")
+        .bind(id ?? 0)
+        .all();
+
+    if (resp.results?.length === 0) {
+        res.raw = Response.redirect(new URL(req.url).origin, 302);
+        return;
+    }
+
+    const result: { content: string, user_id: number } = resp.results!.at(0) as { content: string, user_id: number };
+    const asset = await getAssetFromKV(req.raw, {
+        ASSET_NAMESPACE: env.__STATIC_CONTENT,
+        ASSET_MANIFEST: assetManifest,
+        mapRequestToAsset: customKeyModifier(id, 'view.html'),
+    })
+
+    try {
+        res.headers.set("Content-Type", "text/html");
+        res.body = Mustache.render(await asset.text()!, { paste: result.content, url: `${new URL(req.url).origin}/${id}`, id });
         return;
     } catch (e) {
         res.headers.set("Content-Type", "text/html");
