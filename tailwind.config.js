779fa515466133321c2ad920576261f6952626aa
SphericalKat <amolele@gmail.com>
2023-02-16 18:57:54 +0530

    feat: add styling
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/**/*.{mustache,html}"],
+    content: ["./public/**/*.{mustache,html,css}"],
   theme: {
     extend: {},
   },
